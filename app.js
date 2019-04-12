be9f2581c44735de7af715cbfe9bf504af9e2fa1
Sophie OT <SophieOT@Sophies-MacBook-Pro.local>
2019-04-11 17:32:47 -0700

    fixed some things

--- app.js
+++ app.js
@@ -5,17 +5,13 @@
 
   // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyDTap89S6zmxVoFPgMjPmJdv_7SOHEgiJA",
-    authDomain: "train-time-e7233.firebaseapp.com",
-    databaseURL: "https://train-time-e7233.firebaseio.com",
-    projectId: "train-time-e7233",
-    storageBucket: "train-time-e7233.appspot.com",
-    messagingSenderId: "303131132155"
+  apiKey: "AIzaSyDTap89S6zmxVoFPgMjPmJdv_7SOHEgiJA",
+  authDomain: "train-time-e7233.firebaseapp.com",
+  databaseURL: "https://train-time-e7233.firebaseio.com",
+  storageBucket: "train-time-e7233.appspot.com",
 };
 
 firebase.initializeApp(config);
-
-
 
 var database = firebase.database();
 
@@ -51,7 +47,7 @@ database.ref().on("child_added", function(snapshot, prevChildKey) {
 
     console.log(prevChildKey);
 
-    var date = moment(snapshot.val().startDate);
+    //var time = moment(snapshot.val().startTime;
     //var monthWorked = Math.abs(date.diff(moment(),"months"));
 
     var tableRow = $("<tr>");
@@ -60,17 +56,17 @@ database.ref().on("child_added", function(snapshot, prevChildKey) {
     var tableName = $("<td>");
     tableRow.append(tableName.text(snapshot.val().name));
 
-    var tableRole = $("<td>");
-    tableRow.append(tableRole.text(snapshot.val().dest));
-
-    var tableDate = $("<td>");
-    tableRow.append(tableDate.text(snapshot.val().startTime));
+    var tableDest = $("<td>");
+    tableRow.append(tableDest.text(snapshot.val().dest));
 
     //var tableWork = $("<td>");
     //tableRow.append(tableWork.text(monthWorked));
 
-    var tableRate = $("<td>");
-    tableRow.append(tableRate.text(snapshot.val().freq));
+    var tableFreq = $("<td>");
+    tableRow.append(tableFreq.text(snapshot.val().freq));
+
+    var tableTime = $("<td>");
+    tableRow.append(tableTime.text(snapshot.val().startTime));
 
    //var tableBill = $("<td>");
     //tableRow.append(tableBill.text(monthWorked*snapshot.val().rate));
